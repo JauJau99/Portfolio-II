7770331eacb59f51f2d3dbd123004a188152c6f1
Mats G. Nordbø <mats.1999@live.no>
2025-11-15 21:38:07 +0100

    Created a game over function, and cleaned up the variables a bit.

--- game.mjs
+++ game.mjs
@@ -1,6 +1,11 @@
 //#region CONSTANTS ------------------------------------------------------------------
 const FPS = 1000 / 60;
-const STATES = { MENU: 1, PLAY: 2, GAMEOVER: 3 }
+const GAME_STATES = {
+  IDLE: 0, 
+  MENU: 1, 
+  PLAY: 2, 
+  GAMEOVER: 3
+}
 
 //#endregion
 
@@ -8,7 +13,7 @@ const STATES = { MENU: 1, PLAY: 2, GAMEOVER: 3 }
 const scene = document.getElementById("scene");
 const brush = getBrush();
 
-let currentState = STATES.IDLE;
+let currentGameState = GAME_STATES.IDLE;
 
 // ------
 
@@ -34,16 +39,19 @@ const ship = {
 
 // ------
 
-const projectieWidth = 3;
-const projectileHeight = 5;
-const projectileSpeed = 2;
-const projectileCooldown = 40;
-let cooldown = 0;
+const PROJECTILE = {
+  width: 3,
+  height: 5,
+  speed: 2,
+  cooldownTime: 40
+}
+
+let projectileCooldown = 0;
 let projectiles = [];
 
 // ------
 
-const NPC = {
+const INVADERS = {
   width: 50,
   height: 20,
   padding: 20,
@@ -54,17 +62,17 @@ const NPC = {
   enteties: []
 }
 
-const npcPerRow = Math.floor((scene.width - NPC.height) / (NPC.width + NPC.height));
+const npcPerRow = Math.floor((scene.width - INVADERS.height) / (INVADERS.width + INVADERS.height));
 
 // ------
 
-// Movment back and forth of NPC´s are govered by counting up to a level
-const maxMovmentSteps = 50;
-let movmentSteps = maxMovmentSteps;
+// Movement back and forth of NPC´s are govered by counting up to a level
+const maxMovementSteps = 50;
+let movementSteps = maxMovementSteps;
 
 // ------
 // The following is a simple way of 
-let controllKeys = {
+let controlKeys = {
   ArrowDown: false,
   ArrowUp: false,
   ArrowLeft: false,
@@ -73,11 +81,11 @@ let controllKeys = {
 }
 
 window.addEventListener("keydown", function (e) {
-  controllKeys[e.key] = true;
+  controlKeys[e.key] = true;
 });
 
 window.addEventListener("keyup", function (e) {
-  controllKeys[e.key] = false;
+  controlKeys[e.key] = false;
 })
 
 
@@ -88,24 +96,24 @@ window.addEventListener("keyup", function (e) {
 
 function init() {
 
-  let x = NPC.sx;
-  let y = NPC.sy;
+  let x = INVADERS.sx;
+  let y = INVADERS.sy;
   for (let i = 0; i < npcPerRow; i++) {
-    NPC.enteties.push({ x, y, color: "Yellow", active: true, width: NPC.width, height: NPC.height });
-    x += NPC.width + NPC.padding;
+    INVADERS.enteties.push({ x, y, color: "Yellow", active: true, width: INVADERS.width, height: INVADERS.height });
+    x += INVADERS.width + INVADERS.padding;
   }
 
-  NPC.speed = 1;
+  INVADERS.speed = 1;
 
-  currentState = STATES.MENU;
+  currentGameState = GAME_STATES.MENU;
   update();
 }
 
 function update(time) {
 
-  if (currentState === STATES.MENU) {
+  if (currentGameState === GAME_STATES.MENU) {
     updateMenu(time);
-  } else if (currentState === STATES.PLAY) {
+  } else if (currentGameState === GAME_STATES.PLAY) {
     updateGame(time);
   }
 
@@ -116,15 +124,28 @@ function update(time) {
 function draw() {
   clearScreen();
 
-  if (currentState === STATES.MENU) {
+  if (currentGameState === GAME_STATES.MENU) {
     drawMenu();
-  } else if (currentState === STATES.PLAY) {
+  } else if (currentGameState === GAME_STATES.PLAY) {
     drawGameState();
+  } else if (currentGameState=== GAME_STATES.GAMEOVER){
+    drawGameOver();
   }
 
 }
 
 init(); // Starts the game
+
+function drawGameOver(){
+  brush.font = "40px serif";
+  brush.fillText("GAME OVER", 100, 150);
+  brush.font = "20px serif";
+  brush.fillText("Trykk SPACE for meny", 100, 200);
+
+  if (controlKeys[" "]) {
+    currentGameState = GAME_STATES.MENU;
+  }
+}
 
 //#endregion
 
@@ -133,14 +154,14 @@ init(); // Starts the game
 
 function updateMenu(dt) {
 
-  if (controllKeys[" "]) {
+  if (controlKeys[" "]) {
     MENU.buttons[MENU.currentIndex].action();
   }
 
 
-  if (controllKeys.ArrowUp) {
+  if (controlKeys.ArrowUp) {
     MENU.currentIndex--;
-  } else if (controllKeys.ArrowDown) {
+  } else if (controlKeys.ArrowDown) {
     MENU.currentIndex++;
   }
 
@@ -171,7 +192,7 @@ function updateGame(dt) {
   updateProjectiles();
   updateInvaders();
   if (isGameOver()) {
-    currentState = STATES.GAMEOVER;
+    currentGameState = GAME_STATES.GAMEOVER;
   }
 }
 
@@ -179,19 +200,19 @@ function updateInvaders() {
 
   let ty = 0;
 
-  if (NPC.direction == 1 && movmentSteps >= maxMovmentSteps * 2) {
-    movmentSteps = 0;
-    NPC.direction *= -1
-  } else if (NPC.direction == -1 && movmentSteps >= maxMovmentSteps * 2) {
-    movmentSteps = 0;
-    NPC.direction *= -1;
-    ty += NPC.height;
+  if (INVADERS.direction == 1 && movementSteps >= maxMovementSteps * 2) {
+    movementSteps = 0;
+    INVADERS.direction *= -1
+  } else if (INVADERS.direction == -1 && movementSteps >= maxMovementSteps * 2) {
+    movementSteps = 0;
+    INVADERS.direction *= -1;
+    ty += INVADERS.height;
   }
 
-  let tx = NPC.speed * NPC.direction;
+  let tx = INVADERS.speed * INVADERS.direction;
 
   for (let i = 0; i < npcPerRow; i++) {
-    let invader = NPC.enteties[i];
+    let invader = INVADERS.enteties[i];
 
     if (invader.active) {
 
@@ -206,12 +227,12 @@ function updateInvaders() {
 
   }
 
-  movmentSteps++;
+  movementSteps++;
 
 }
 
 function isGameOver() {
-  for (let invader of NPC.enteties) {
+  for (let invader of INVADERS.enteties) {
     if (invader.active) {
       return false;
     }
@@ -234,9 +255,9 @@ function isShot(target) {
 }
 
 function updateShip() {
-  if (controllKeys.ArrowLeft) {
+  if (controlKeys.ArrowLeft) {
     ship.velocityX--;
-  } else if (controllKeys.ArrowRight) {
+  } else if (controlKeys.ArrowRight) {
     ship.velocityX++;
   }
 
@@ -247,20 +268,27 @@ function updateShip() {
 
   ship.x = tmpX;
 
-  cooldown--;
+  projectileCooldown--;
 
-  if (controllKeys[" "] && cooldown <= 0) {
-    projectiles.push({ x: ship.x + ship.width * 0.5, y: ship.y, dir: -1, active: true, width: projectieWidth, height: projectileHeight });
-    cooldown = projectileCooldown;
+  if (controlKeys[" "] && projectileCooldown <= 0) {
+    projectiles.push({
+      x: ship.x + ship.width * 0.5, 
+      y: ship.y, 
+      dir: -1, 
+      active: true, 
+      width: PROJECTILE.width, 
+      height: PROJECTILE.height
+    });
+    projectileCooldown = PROJECTILE.cooldownTime;
   }
 }
 
 function updateProjectiles() {
   let activeProjectiles = []
   for (let i = 0; i < projectiles.length; i++) {
-    let projectile = projectiles[i]
-    projectile.y += projectileSpeed * projectile.dir;
-    if (projectile.y + projectileHeight > 0 && projectile.active) {
+    let projectile = projectiles[i];
+    projectile.y += PROJECTILE.speed * projectile.dir;
+    if (projectile.y + projectile.height > 0 && projectile.active) {
       activeProjectiles.push(projectile);
     }
   }
@@ -274,21 +302,21 @@ function drawGameState() {
 
   for (let projectile of projectiles) {
     if (projectile.active) {
-      brush.fillRect(projectile.x, projectile.y, projectieWidth, projectileHeight);
+      brush.fillRect(projectile.x, projectile.y, projectile.width, projectile.height);
     }
   }
 
   for (let i = 0; i < npcPerRow; i++) {
-    let invader = NPC.enteties[i];
+    let invader = INVADERS.enteties[i];
     if (invader.active) {
       brush.fillStyle = invader.color;
-      brush.fillRect(invader.x, invader.y, NPC.width, NPC.height);
+      brush.fillRect(invader.x, invader.y, INVADERS.width, INVADERS.height);
     }
   }
 }
 
 function startPlay() {
-  currentState = STATES.PLAY;
+  currentGameState = GAME_STATES.PLAY;
 }
 
 function showHigScores() {
